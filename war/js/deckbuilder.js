ea618e6efcac97dff8aee06531960a51f99f342a
Russell Leggett <russell.leggett@gmail.com>
2011-08-09 08:22:52 -0400

    updated deck builder to support add/delete and P/T

--- war/js/deckbuilder.js
+++ war/js/deckbuilder.js
@@ -1,5 +1,8 @@
 (function($){
 	$(document).ready(function(){
+		/*
+		 * Set up the data table, defining columns, and using the rest api
+		 */
 		var table = $("#example").dataTable({
 			bProcessing:true,
 			bJQueryUI:true,
@@ -16,6 +19,7 @@
 				   return "<a class='card-name' title='" + card.text + "' href='#'>" + card.name + "</a>";
 			   }},
 	           {sTitle:"Type", mDataProp:"card.types"},
+	           {sTitle:"P/T", mDataProp:"card.powerToughness", sWidth:"40px"},
 	           {sTitle:"Cost", mDataProp:"card.cost", sWidth:"50px"},
 	           {sTitle:"Color", mDataProp:"card.color", sWidth:"80px"},
 	           {sTitle:"Count", mDataProp:"deckCard.count", sWidth:"50px", bUseRendered:false, fnRender:function(row){
@@ -24,7 +28,9 @@
 			   }}
 	        ]
 		});
+		//template for the card popup
 		var viewerTemplate = $("#card-template").template();
+		//set up the dialog and attach open even to name
 		$("#card-viewer").dialog({autoOpen:false});
 		$("#example .card-name").live("click",function(){
 			//debugger;
@@ -36,9 +42,36 @@
 			$("#card-viewer").dialog("open");
 			return false;
 		});
+		
+		//handler for deleting the row
 		$("#example .remove-card").live("click",function(){
+			var rowElem = this.parentNode.parentNode;
+			var data = table.fnGetData(rowElem);//tr parent of delete div
+			//now that I have the data, lets kill the row, more responsive that way
+			table.fnDeleteRow(rowElem);
+			//now delete on server
+			$.ajax({
+				url:"/rest/deckcards/" + data.deckCard.id,
+				type:"DELETE"
+			});
+			//alert("Deleting " + data.card.name);
+			return false;
+		});
+		
+		//handler for updating count
+		$("#example .card-count").live("change",function(){
+			var updated = $(this).val();
 			var data = table.fnGetData(this.parentNode.parentNode);//tr parent of delete div
-			alert("Deleting " + data.card.name);
+			var deckCard = data.deckCard;
+			deckCard.count = updated;
+			//now delete on server
+			$.ajax({
+				url:"/rest/deckcards/" + deckCard.id,
+			    contentType: "application/json",
+				type:"PUT",
+				data:JSON.stringify(deckCard)
+			});
+			//alert("Deleting " + data.card.name);
 			return false;
 		});
 	});
