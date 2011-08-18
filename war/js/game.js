abc4136fe8d9ff8887d466c96b6c2e80c8ae2af9
Russell Leggett <russell.leggett@gmail.com>
2011-08-17 23:43:15 -0400

    updated to read game id

--- war/js/game.js
+++ war/js/game.js
@@ -8,14 +8,19 @@ App.MyView = SC.View.extend({
 
 //(function($){
 //	$(document).ready(function(){
+//		//get the game id - not provided so game can be completely static
+//		var path = window.location.pathname; //should look like /game/{id}
+//		var id = path.substring(6);
+//		
 //		$.ajax({
-//			url:"http://bandinglanding.appspot.com/rest/game/16003",
+//			url:"http://bandinglanding.appspot.com/rest/game/" + id,
 //		    contentType: "application/json",
 //			type:"GET",
 //			xhrFields: {
 //				withCredentials: true
 //			},
 //			success:function(data){
+//				alert(data.playerDto.player.user.email + " is the player!");
 //				console.log(data);
 //			}
 //		});
